c8212d78a0150782dee8a8745314b08f70b5d791
deniseurbanija2 <deniseurbanija@outlook.com.ar>
2024-03-05 10:53:54 -0300

    lecture 1

--- front/scripts/index.js
+++ front/scripts/index.js
@@ -1,1 +1,38 @@
 console.log(tempData);
+
+const cardContainer = document.querySelector(".cardContainer");
+
+const convert = (tempData) => {
+  const { poster, title, year, duration } = tempData;
+
+  const moviePoster = document.createElement("img");
+  moviePoster.classList.add("poster");
+  moviePoster.src = poster;
+
+  const movieTitle = document.createElement("a");
+  movieTitle.classList.add("title");
+  movieTitle.href = "";
+  movieTitle.innerHTML = title;
+
+  const movieYear = document.createElement("p");
+  movieYear.classList.add("year");
+  movieYear.innerHTML = year;
+
+  const movieDuration = document.createElement("p");
+  movieDuration.classList.add("duration");
+  movieDuration.innerHTML = duration;
+
+  const card = document.createElement("div");
+  card.classList.add("card");
+  card.append(moviePoster, movieTitle, movieDuration, movieYear);
+
+  return card;
+};
+
+const arrayHTML = tempData.map((movie) => {
+  return convert(movie);
+});
+
+arrayHTML.forEach((element) => {
+  cardContainer.appendChild(element);
+});
