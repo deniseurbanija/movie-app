837131a04bd10c09032bc9c762d933990b138b8c
deniseurbanija2 <deniseurbanija@outlook.com.ar>
2024-03-12 15:24:13 -0300

    express added

---
+++ back/src/controllers/index.js
@@ -0,0 +1,5 @@
+const indexController = (req, res) =>{
+    res.status(200).send("esta funcionando indexController")
+}
+
+module.exports = indexController
---
+++ back/src/routes/index.js
@@ -0,0 +1,8 @@
+const { Router } = require("express");
+const indexController = require("../controllers/index");
+
+const router = Router();
+
+router.get("/", indexController);
+
+module.exports = router;
